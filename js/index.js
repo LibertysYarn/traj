d96b0e0820724a3dc490b49c225f47034a48b96c
libertysyarn <code@libertysyarn.com>
2017-02-23 21:28:18 -0500

    added math.js

--- js/index.js
+++ js/index.js
@@ -13,10 +13,43 @@ $('#setUnitMetric').click(function() {
 document.getElementById("InputTargetWidth").defaultValue = 18;
 document.getElementById("InputTargetHeight").defaultValue = 24;
 
+let eQ = {
+  MilWidth: 0,
+  MilHeight: 0,
+  TargetWidth: [],
+  TargetHeight: [],
+  range: 0,
+  dial: 0
+};
+
 
 $('.unit').click(function() {
-  var addon = $('.input-group-addon');
   var unit = '';
-   unit += $(this).text();
-   thisAdd.text(unit);
- });
+  unit += $(this).text();
+  var addonText = $(event.target).closest('.input-group').find('.input-group-addon').data('name');
+  var addon = $(event.target).closest('.input-group').find('.input-group-addon');
+  var typeOfThing = $(event.target).closest('.input-group').find('.form-control').data('name');
+  var value = $(event.target).closest('.input-group').find('.form-control').val();
+  addon.text(unit + ' ' + addonText);
+
+  var x   = math.unit(value, unit);
+  var str = [x];
+  eQ[typeOfThing] = str;
+  console.log(str);
+  console.log(typeOfThing + ' ' + value + ' ' + unit);
+  console.log(eQ);
+
+});
+
+// (InputTargetHeight (to inches) * 27.778 ) / mil --> range in yards
+
+function convertToInches() {
+  for (var prop in eQ) {
+
+  }
+}
+
+  $('#submit').click(function() {
+
+
+  });
